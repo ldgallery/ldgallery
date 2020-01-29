d16d94cba16ac9972c9a9a65b1a25c8725eaf66f
Zero~Informatique <contact@zeroinformatique.fr>
2020-01-29 07:54:03 +0100

    viewer: improved the picture layout. clicking a picture will switch between resized-to-screen and original-size (+fullscreen). drag-n-drop scrolls/moves the picture, just like a touch device. Resolves #19

--- viewer/src/plugins/fontawesome.ts
+++ viewer/src/plugins/fontawesome.ts
@@ -22,7 +22,6 @@ import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
-    faExpandArrowsAlt,
     faFolder,
     faSearch,
     faTag,
@@ -32,7 +31,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
-    faExpandArrowsAlt,
     faFolder,
     faSearch,
     faTag,
