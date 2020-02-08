d2ed5f271f49822c9e87058673a006ac80a73876
Zero~Informatique <contact@zeroinformatique.fr>
2020-02-08 04:37:22 +0100

    compiler: minor improvements to the CMD scripts

--- compiler/win_compile_example.cmd
+++ compiler/win_compile_example.cmd
@@ -1,10 +1,9 @@
 @echo off
 SET rebuild=--rebuild
-CHOICE /M "Rebuild all?"
+CHOICE /M "Rebuild all ('--rebuild' argument)?"
 IF ERRORLEVEL 2 SET rebuild=
 echo.
-echo stack exec ldgallery-compiler-exe -- %rebuild% -i=../example/ -o=../example/out/
-echo.
-stack exec ldgallery-compiler-exe -- %rebuild% -i=../example/ -o=../example/out/
-echo.
-pause
+
+@echo on
+stack exec ldgallery-compiler-exe -- %rebuild% --clean-output -i=../example/ -o=../example/out/
+@pause
