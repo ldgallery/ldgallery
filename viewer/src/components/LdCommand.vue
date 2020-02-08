b8160585c4ea0037772c6a35c45b58d48621cd4d
pacien <pacien.trangirard@pacien.net>
2020-02-08 18:09:36 +0100

    viewer: ldcommand: fix keyboard navigation
    By using proper semantics, using 'a' instead of 'div' to allow keyboard selection

--- viewer/src/components/LdCommand.vue
+++ viewer/src/components/LdCommand.vue
@@ -19,15 +19,15 @@
 
 <template>
   <div class="flex command-btns">
-    <div class="link" :title="$t('title.tags')" @click="$uiStore.toggleFullWidth()">
+    <a class="link" :title="$t('title.tags')" @click="$uiStore.toggleFullWidth()">
       <fa-icon :icon="commandTagsIcon()" size="lg" />
-    </div>
+    </a>
     <router-link to="/" :class="{'disabled': isRoot()}" :title="$t('title.home')">
       <fa-icon icon="home" size="lg" />
     </router-link>
-    <div class="link" :title="$t('title.back')" @click="$router.go(-1)">
+    <a class="link" :title="$t('title.back')" @click="$router.go(-1)">
       <fa-icon icon="arrow-left" size="lg" />
-    </div>
+    </a>
     <router-link :class="{'disabled': isRoot()}" :title="$t('title.parent')" :to="parent()">
       <fa-icon icon="folder" size="xs" />
       <fa-icon icon="level-up-alt" size="lg" />
